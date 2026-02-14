af5a5a41a02fdebc3dd5ba388ac12cf4c77faa68
Ankit <ankommalapati@gmail.com>
2026-02-14 21:00:00 +0530

    signedin and signedout clauses

--- receiptstack-build/components/Header.tsx
+++ receiptstack-build/components/Header.tsx
@@ -4,15 +4,37 @@ import React from 'react'
 import { ReceiptText } from 'lucide-react'
 import { Button } from './ui/button'
 import Link from 'next/link';
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
+import { usePathname } from 'next/navigation';
 
 function Header() {
+    const pathname = usePathname();
+    const isHomePage = pathname === '/';
   return (
-    <div className='p-4 flex justify-between items-center'>
+    <div className={`p-4 flex justify-between items-center ${
+        isHomePage ? 'bg-blue-50' : 'bg-white border-b border-blue-50'
+        }`}
+    >
         <Link href='/' className='flex items-center'>
             <ReceiptText className='w-6 h-6 text-green-900 mr-3'/>
             <h1 className='text-xl font-semibold'>ReceiptStack</h1>
         </Link>
-        <div></div>
+        <div className='flex items-center space-x-4'>
+            <SignedIn>
+                <Link href='/receipts'>
+                    <Button variant='outline'>My Receipts</Button>
+                </Link>
+                <Link href='/manage-plan'>
+                    <Button>Manage Plan</Button>
+                </Link>
+                <UserButton />
+            </SignedIn>
+            <SignedOut>
+                <SignInButton mode='modal'>
+                    <Button>Log In</Button>
+                </SignInButton>
+            </SignedOut>
+        </div>
     </div>
   )
 }
